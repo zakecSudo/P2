12f15fe8c8e6ede4ed192ca67e5944a10feb7e80
Zakec <zakalbreht30@gmail.com>
2020-03-29 21:12:37 +0200

    test

--- src/files/naloga05/DN05.java
+++ src/files/naloga05/DN05.java
@@ -11,7 +11,6 @@ public class DN05 {
         //izrisiLabirint(preberiLabirint(args[0]));
         //preberiResitev(args[1]);
         //preveriResitev(preberiLabirint(args[0]), preberiResitev(args[1]));
-        System.out.println("");
         izrisiLabirint(narediLabirint(10,10, 1));
 
     }
